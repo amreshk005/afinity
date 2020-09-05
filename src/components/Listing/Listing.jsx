dd0661e26a0aeba01a8ab465f0f68b2ecb9ce341
ABHISHEK KUMAR <amreshk005@gmail.com>
2020-09-05 12:51:47 +0530

    responsive done

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -39,9 +39,9 @@ class Listing extends Component {
       });
 
     return (
-      <div className="row justify-content-between flex-nowrap m-0 mt-4">
-        <div className="col-4">
-          <div className="col border" style={{ minHeight: "85vh" }}>
+      <div className="row m-0 mt-4 flex-column flex-sm-column flex-md-column  flex-lg-row justify-content-between flex-nowrap">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-4">
+          <div className="col border h-100 d-flex flex-md-row flex-sm-row flex-lg-column  justify-content-sm-around justify-content-lg-start">
             {[...Object.keys(getCategory), "ShowAll"].map((e) => (
               <div key={uuidv4()} className="card m-2">
                 <div name={e} className="card-body text-capitalize " style={{ cursor: "pointer" }} onClick={this.FilterHandler}>
@@ -51,8 +51,8 @@ class Listing extends Component {
             ))}
           </div>
         </div>
-        <div className="col-8 border p-0">
-          <div className="col p-0">
+        <div className="col-lg-8 col-sm-12 border p-0">
+          <div className="col p-0 ">
             <div className="card m-3 border-0">
               <h5 className="card-title text-capitalize">Products under {this.state.filter}</h5>
             </div>
@@ -61,16 +61,18 @@ class Listing extends Component {
                 <div> Loading...</div>
               ) : (
                 products.map((item) => (
-                  <div key={uuidv4()} className="card ml-2 mt-3 flex-nowrap" style={{ width: "23%", height: "20rem" }}>
-                    <img className="card-img-top h-50" src={item.product_image} alt="Card cap" />
-                    <div className="card-body d-flex flex-start flex-column p-1 justify-content-between">
-                      <div className="p-3">
-                        <h5 className="card-title text-capitalize">{item.product_name}</h5>
-                        <p>Price: {item.price}</p>
+                  <div key={uuidv4()} className="mt-3 flex-nowrap w-sm-50 col-5 col-sm-3 col-md-3 ml-3 ml-sm-3 ml-md-3 ml-lg-0" style={{ height: "20rem" }}>
+                    <div className="card border h-100">
+                      <img className="card-img-top h-50" src={item.product_image} alt="Card cap" />
+                      <div className="card-body d-flex flex-start flex-column p-1 justify-content-between">
+                        <div className="p-3">
+                          <h5 className="card-title text-capitalize">{item.product_name}</h5>
+                          <p>Price: {item.price}</p>
+                        </div>
+                        <button className="btn btn-dark pl-4 pr-5 text-center rounded-0" onClick={() => this.cartHandler(item)}>
+                          Add to Cart
+                        </button>
                       </div>
-                      <button className="btn btn-dark pl-4 pr-5 text-center rounded-0" onClick={() => this.cartHandler(item)}>
-                        Add to Cart
-                      </button>
                     </div>
                   </div>
                 ))
